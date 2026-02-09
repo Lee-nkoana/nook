20212a903ebb776bf1bdee2430bfcab857843048
Lebohang Nkoana <lenkoanjhb024@student.wethinkcode.co.za>
2026-02-09 14:12:57 +0200

    created new app for dashboard

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# nook
+# Nook
+
+Nook is your online book corner. Built to make finding books, storing notes and rating books easy and accessible and convinient. 
+
+## 🚀 Features
+- **User authentification and profiles** - User are able to create secure profiles.
+- **Dashboard** - Users can view their saved books, book ratings, in progress reads, and their sticky notes.
+- **Sticky Notes** - Users are able to add comments and notes about the books the have read.
+
+## 🧰 Tech Stack
+- **Python**
+- **Django**
+- **HTML / CSS / JavaScript**
+- **PostgreSQL**
+- **REST APIs**
+- **Bootstrap / Tailwind CSS**
+
+## 🔧 Installation
+
+1. **Clone the repo**
+
+   ```bash
+   git clone https://github.com/Lee-nkoana/nook.git
+   cd nook
+   ```
+
+2. ## Create & activate a virtual environment
+
+   ```bash
+   python -m venv venv
+   source venv/bin/activate  # Mac/Linux
+   venv\Scripts\activate     # Windows
+   ```
+
+3. ## Install dependencies
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+4. ## How to run the app
+   ```bash
+   python manage.py runserver
+   flask run
+   ```
+
+## 🧑‍💻 Contributing
+
+Contributions are more than welcome!
+If you have ideas, issues, or want to contribute code, feel free to open a pull request or issue.
+
+Refer to the CONTRIBUTIONS.md for git commit message structures.
+
+## Documentation
+
+Check project wiki for detailed documentation: https://github.com/Lee-nkoana/nook/wiki
--- members/views.py
+++ members/views.py
@@ -1,5 +1,53 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.db import IntegrityError
+
+from .models import User
 
 # Create your views here.
 def index(request):
     return render(request, "members/index.html")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.Post['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "members/login.html", {
+                "message": "Invalid username and/ or password"
+            })
+    else:
+        return render(request, "members/login.html")
+    
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+    
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        confirm_password = request["confirm_password"]
+
+        if password != confirm_password:
+            return render(request, "members/register.html", {
+                "message": "Passwords must match."
+            })
+        
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "members/register.html",{
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(render(reverse("index")))
